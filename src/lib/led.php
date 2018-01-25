85b183c4fd3d668aaefec0f8a332de31f3f82dad
xubeiyan <yueyongdong@gmail.com>
2018-01-25 22:47:04 +0800

    add GET/POST handler in led.php...

--- src/lib/led.php
+++ src/lib/led.php
@@ -20,31 +20,75 @@ class LED {
 	 */
 	public static function start() {
 		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-			if (isset($_GET['login'])) {
-				if (isset($_SESSION['login']) && $_SESSION['login'] == true) {
-					header('refresh:0;url=.');
-				}
-				
-				$login_templates = Array(
-					'header_title' => '登录',
-					'title' => '标准查询系统 - 登录',
-					'script' => 'templates/js/login.js',
-				);
-				self::render('login', $login_templates);
-			} else if (isset($_GET['install'])) {
-				$install_templates = Array(
-					'header_title' => '安装',
-				);
-				self::render('install', $install_templates);
-			}
+			self::get_handler();
 		} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$request = file_get_contents('php://input');
-			print_r($request);
-			exit();
+			self::post_handler();
 		}
 		
 		// 什么都获取不到则渲染主页
 		self::render('index');
+	}
+	
+	/**
+	* 处理get请求
+	*/
+	private static function get_handler() {
+		if (isset($_GET['login'])) {
+			if (isset($_SESSION['login']) && $_SESSION['login'] == true) {
+				header('refresh:0;url=.');
+			}
+			
+			$login_templates = Array(
+				'header_title' => '登录',
+				'title' => '标准LED查询系统 - 登录',
+				'script' => 'templates/js/login.js',
+			);
+			self::render('login', $login_templates);
+		} else if (isset($_GET['install'])) {
+			$install_templates = Array(
+				'header_title' => '安装',
+			);
+			self::render('install', $install_templates);
+		}
+	}
+	
+	/**
+	* 处理post请求
+	*/
+	private static function post_handler() {
+		$request = file_get_contents('php://input');
+		
+		$decode_req = json_decode($request, TRUE);
+		
+		if ($decode_req == FALSE) {
+			self::error('json_parse_error');
+		}
+		
+		if (!isset($decode_req['request'])) {
+			self::error('request_parm_miss_error');
+		}
+		
+		if ($decode_req['request'] == 'login') {
+			$expect_array = Array(
+				'username' => Array(
+					'regexp' => '/^[A-Za-z0-9]{1,32}$/', // A-Za-z0-9的1到32位
+				),
+				'password' => Array(
+					'regexp' => '/^[\w]{6,32}$/', // \w的6-32位
+				),
+			);
+			$result = self::expect($expect_array, $decode_req);
+			if ($result != 'pass') {
+				self::error('param_error', $result);
+			}
+			
+			$success_info = Array(
+				'session' => '12345678',
+			);
+			header('Content-Type: application/json');
+			echo json_encode($success_info, JSON_UNESCAPED_UNICODE); 
+		}
+		exit();
 	}
 	
 	/**
@@ -81,10 +125,10 @@ class LED {
 		$layout_content = str_replace('{{ footer }}', $footer_content, $layout_content);
 		
 		$entries = Array (
-			'title' 		=> '标准查询系统',
+			'title' 		=> '标准LED查询系统 - 首页',
 			'header_title' 	=> '首页',
 			'css_file' 		=> 'templates/css/main.css',
-			'script' 		=> '1.js',
+			'script' 		=> 'templates/js/index.js',
 			'date'			=> date('Y年m月d日'),
 		);
 		
@@ -111,6 +155,48 @@ class LED {
 		echo $result;
 		echo $time_used;
 		exit();
+	}
+	
+	/**
+	* 输出错误信息
+	*/
+	private static function error($err_str, $err_info = '', $type = 'json') {
+		if ($err_str == 'json_parse_error') {
+			$err_msg = 'JSON解析错误';
+		} else if ($err_str == 'request_parm_miss_error') {
+			$err_msg = '请求中缺少request字段';
+		} else if ($err_str == 'param_error') {
+			$err_msg = $err_info;
+		} else {
+			$err_msg = '未知错误';
+		}
+		
+		if ($type == 'json') {
+			$return_array = Array (
+				'err_type' => $err_str,
+				'err_msg' => $err_msg,
+			);
+			
+			header('Content-Type: application/json');
+			echo json_encode($return_array, JSON_UNESCAPED_UNICODE);
+			exit();
+		}
+	}
+	
+	/**
+	* 检查字段
+	*/
+	private static function expect($expect, $request) {
+		foreach ($expect as $key => $value) {
+			if (!isset($request[$key])) {
+				return 'not set key: ' . $key;
+			}
+			
+			if (preg_match($value['regexp'], $request[$key]) == 0) {
+				return 'not match regexp: ' . $key;
+			}
+		}
+		return 'pass';
 	}
 }
 ?>
